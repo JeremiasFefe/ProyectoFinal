6f4a1e21cbd62a01d5eaed62b436fee18e656c03
Jere Fefe <jerefefe@gmail.com>
2020-05-25 21:36:26 -0300

    Objeto app creado (con nombre y array de funcionalidades)

---
+++ app/src/main/java/com/example/proyectofinal/App.kt
@@ -0,0 +1,12 @@
+package com.example.proyectofinal
+
+class App (name: String, functionalities: ArrayList<String>){
+    lateinit var name:String
+    lateinit var functionalities:ArrayList<String>
+
+    init {
+        this.name = name
+        this.functionalities = functionalities
+    }
+
+}
