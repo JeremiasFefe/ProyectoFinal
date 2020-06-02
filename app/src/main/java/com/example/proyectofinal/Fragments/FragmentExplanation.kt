859e131279d0dd79162b1f39d3e7b2107f4dec31
Jere Fefe <jerefefe@gmail.com>
2020-06-02 18:24:17 -0300

    Dropdown menu creado

--- app/src/main/java/com/example/proyectofinal/Fragments/FragmentExplanation.kt
+++ app/src/main/java/com/example/proyectofinal/Fragments/FragmentExplanation.kt
@@ -1,10 +1,9 @@
 package com.example.proyectofinal.Fragments
 
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import android.widget.TextView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.example.proyectofinal.R
 import kotlinx.android.synthetic.main.fragment_explanation.*
@@ -32,5 +31,28 @@ class FragmentExplanation : Fragment() {
 
         txtFuncName.text = functionalitySelected
         txtAppName.text = appSelected
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        setHasOptionsMenu(true)
+        super.onCreate(savedInstanceState)
+    }
+
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.main_menu,menu)
+        super.onCreateOptionsMenu(menu, inflater)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+        when (item.itemId) {
+            R.id.settings_item -> Toast.makeText(activity, "settings", Toast.LENGTH_SHORT).show()
+            R.id.info_item -> Toast.makeText(activity, "info", Toast.LENGTH_SHORT).show()
+            R.id.contact_item -> Toast.makeText(activity, "info", Toast.LENGTH_SHORT).show()
+            else -> Toast.makeText(activity,"not working", Toast.LENGTH_SHORT).show()
+        }
+        return super.onOptionsItemSelected(item)
+
     }
 }
--- app/src/main/java/com/example/proyectofinal/Fragments/FragmentFunctionalities.kt
+++ app/src/main/java/com/example/proyectofinal/Fragments/FragmentFunctionalities.kt
@@ -1,12 +1,11 @@
 package com.example.proyectofinal.Fragments
 
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import android.widget.BaseAdapter
 import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.navigation.Navigation
 import androidx.navigation.findNavController
@@ -53,6 +52,28 @@ class FragmentFunctionalities : Fragment() {
     override fun onStart() {
         super.onStart()
 
+
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        setHasOptionsMenu(true)
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.main_menu,menu)
+        super.onCreateOptionsMenu(menu, inflater)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+        when (item.itemId) {
+            R.id.settings_item -> Toast.makeText(activity, "settings", Toast.LENGTH_SHORT).show()
+            R.id.info_item -> Toast.makeText(activity, "info", Toast.LENGTH_SHORT).show()
+            R.id.contact_item -> Toast.makeText(activity, "info", Toast.LENGTH_SHORT).show()
+            else -> Toast.makeText(activity,"not working", Toast.LENGTH_SHORT).show()
+        }
+        return super.onOptionsItemSelected(item)
 
     }
 }
