3e866c13403f06d9b69ac2c0c2aa97f70f3af310
JeremiasFefe <60411496+JeremiasFefe@users.noreply.github.com>
2020-08-21 23:13:53 -0300

    Boton de descarga de las apps agregado

--- app/src/main/java/com/example/proyectofinal/Services/DataService.kt
+++ app/src/main/java/com/example/proyectofinal/Services/DataService.kt
@@ -6,15 +6,17 @@ import com.example.proyectofinal.Model.Functionality
 
 object DataService {
     val apps = listOf(
-            App("WhatsApp","whatsapp"),
-            App("YouTube","youtube"),
-            App("Netflix","netflix"),
-            App("Google Maps","maps")
+            App("WhatsApp","whatsapp","com.whatsapp"),
+            App("YouTube","youtube","com.google.android.youtube"),
+            App("Netflix","netflix","com.netflix.mediaclient"),
+            App("Google Maps","maps","com.google.android.apps.maps")
             //App("Google Chrome","chrome")
     )
-
+    private val blankStepsList= listOf(
+            ExplanationStep("","","","")
+    )
     private val mapsBarraBuscadora = listOf(
-            ExplanationStep("1","https://firebasestorage.googleapis.com/v0/b/aprendapp-7fba8.appspot.com/o/googlemaps_1.jpeg?alt=media&token=3b98aadc-277e-4cdd-aa83-658947c643a3","https://firebasestorage.googleapis.com/v0/b/aprendapp-7fba8.appspot.com/o/googlemaps_1_mask.jpg?alt=media&token=c398ebe0-c899-4cea-837f-72820d77b509",""),
+            ExplanationStep("1","https://firebasestorage.googleapis.com/v0/b/aprendapp-7fba8.appspot.com/o/giftest.gif?alt=media&token=bc119553-2b49-46d0-96a9-fc4c8feb3c7f","https://firebasestorage.googleapis.com/v0/b/aprendapp-7fba8.appspot.com/o/googlemaps_1_mask.jpg?alt=media&token=c398ebe0-c899-4cea-837f-72820d77b509",""),
             ExplanationStep("2","https://firebasestorage.googleapis.com/v0/b/aprendapp-7fba8.appspot.com/o/googlemaps_1.jpeg?alt=media&token=3b98aadc-277e-4cdd-aa83-658947c643a3","https://firebasestorage.googleapis.com/v0/b/aprendapp-7fba8.appspot.com/o/googlemaps_1_mask.jpg?alt=media&token=c398ebe0-c899-4cea-837f-72820d77b509","")
     )
 
@@ -29,33 +31,38 @@ object DataService {
             Functionality("Uso general", whatsappUsoGeneralExplanation),
             Functionality("Mandar mensaje",whatsappUsoGeneralExplanation),
             Functionality("Grabar audio",whatsappUsoGeneralExplanation),
-            Functionality("Hacer llamada",whatsappUsoGeneralExplanation)
+            Functionality("Hacer llamada",whatsappUsoGeneralExplanation),
+            Functionality("Descargar", blankStepsList)
     )
 
 
     private val youtubeFunc = listOf(
             Functionality("Uso general",whatsappUsoGeneralExplanation),
             Functionality("Buscar video",whatsappUsoGeneralExplanation),
-            Functionality("Explorar",whatsappUsoGeneralExplanation)
+            Functionality("Explorar",whatsappUsoGeneralExplanation),
+            Functionality("Descargar", blankStepsList)
     )
 
 
     private val mapsFunc = listOf(
             Functionality("Uso general", mapsBarraBuscadora),
             Functionality("Buscar dirección",whatsappUsoGeneralExplanation),
-            Functionality("Buscar lugar",whatsappUsoGeneralExplanation)
+            Functionality("Buscar lugar",whatsappUsoGeneralExplanation),
+            Functionality("Descargar", blankStepsList)
     )
 
 
     private val chromeFunc = listOf(
             Functionality("Uso general",whatsappUsoGeneralExplanation),
-            Functionality("Usar buscador",whatsappUsoGeneralExplanation)
+            Functionality("Usar buscador",whatsappUsoGeneralExplanation),
+            Functionality("Descargar", blankStepsList)
     )
 
     private val netflixFunc = listOf(
             Functionality("Uso general",whatsappUsoGeneralExplanation),
             Functionality("Buscar película",whatsappUsoGeneralExplanation),
-            Functionality("Tv",whatsappUsoGeneralExplanation)
+            Functionality("Tv",whatsappUsoGeneralExplanation),
+            Functionality("Descargar", blankStepsList)
     )
 
     fun getFunctionalities(app: String): List<Functionality>{
